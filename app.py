5ea55e257e9ad9ff5ae899f9a45be1dbff016f15
AmjadAlakrami <43879892+AmjadAlakrami@users.noreply.github.com>
2019-05-01 00:15:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,44 @@
-from tkinter import *
-import tkinter as ttk
-from ttk import *
+from tkinter import * #importera allt from tkinter
 
+def pr():
+    print("Hallo")
 root = Tk()
+root.title("Quize") #Namner på Tk fönstret
 
-topFrame = Frame(root)
-topFrame.pack()
+topFrame = Frame(root)#skapar en  variabel och definerar den som en frame 
+topFrame.pack()#packar in det alltså lägger in det i vårt fönser
 bottomFrame = Frame(root)
 bottomFrame.pack(side=BOTTOM)
 
-label1 = Label( topFrame, text="Välj antal frågor :")
-label1.grid(row=0, sticky=E)
 
-label2 = Label( topFrame, text="Välj svårighetsgraden :")
+label1 = Label( topFrame, text="How many questions? ") #skapar en taxtrutta, lägger in det i topframe som vi skapade 
+label1.grid(row=0, sticky=E)#lägger den på rad 0 "första raden", så nära komponenten som ligger höger om textruttan
+
+label2 = Label( topFrame, text="Select difficulty :")
 label2.grid(row=1, sticky=E)
 
-label3 = Label( topFrame, text="Välj Katigorien :")
+label3 = Label( topFrame, text="Select Category :")
 label3.grid(row=2, sticky=E )
 
-TextINP1= Entry(topFrame, )
-TextINP1.grid(row=0, column=1 )
+TextINP1= Entry(topFrame) #skapar en text input
+TextINP1.grid(row=0, column=1, sticky=W )# rad 0 kolumn 1 
 
-# Checkbox1= Checkbutton(topFrame, text="Medium")
-# Checkbox1.grid(row=1, column=1, sticky=E)
-# Checkbox2= Checkbutton(topFrame, text="Easy" )
-# Checkbox2.grid(row=1, column=1, sticky=W
-# Checkbox3= Checkbutton(topFrame, text="Hard" )
-# Checkbox3.grid(row=1, column=2, sticky=W)
-# Checkbox4= Checkbutton(topFrame, text="Random" )
-# Checkbox4.grid(row=1, column=3, sticky=W)
+#skapar en dropdown menu
+Level = [ "Easy","Medium","Hard", "Random"] #en dictionary på de variabler vi vill ha i menyn
+DefultVar = StringVar(root) #skapar en string variabel 
+DefultVar.set(Level[0])#sätter variabeln till den första namnet i dictionary
+popupMenu = OptionMenu(topFrame, DefultVar, *Level)#skapar en variabel och definerar den som en dropdown menu "OptionMenu", och vi säger att det ska var i topfram, strat värdet ska var string variabel vi skapade och den ska innehålla dictionaryt vi skapade 
+popupMenu.grid(row=1, column=1, sticky=W)#positioner 
 
-TextINP3= Entry(topFrame, )
-TextINP3.grid(row=2, column=1 )
 
-knapp = Button(bottomFrame, text="Submit")
-knapp.pack()
-# Create a Tkinter variable
+Category = [ "Entertainmetn: Music","Entertainmetn: Video Games","Entertainmetn: Film", "Science: Mathematic", "Science: Computers", "Sport", "General knowledge", "Random"]
+DefultVar2 = StringVar(root)
+DefultVar2.set(Category[0])
+popupMenu2 = OptionMenu(topFrame, DefultVar2, *Category)
+popupMenu2 .grid(row=2, column=1, sticky=W )
 
-choices = [ "dasds","Medium","Hard"]
-tkvar = StringVar()
-tkvar.set(choices[1])
-popupMenu = OptionMenu(root, tkvar, *choices)
-popupMenu.pack()
-root.mainloop()
+knapp = Button(bottomFrame, text="Submit")#skapar en knapp
+knapp.grid(row=11)
+
+
+root.mainloop()#kör programmet helt tiden till vi breakar loopen. 
