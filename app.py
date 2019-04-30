bb4e60e593f2a34f93d4b0960c5f723c4c4f1628
AmjadAlakrami <amjad.alakrmi@abbbindustrigymnasium.se>
2019-04-30 21:08:35 +0200

    mn

---
+++ app.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+root = Tk()
+
+topFrame = Frame(root)
+topFrame.pack()
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+label1 = Label( topFrame, text="Välj antal frågor :")
+label1.grid(row=0, sticky=E)
+
+label2 = Label( topFrame, text="Välj svårighetsgraden :")
+label2.grid(row=1, sticky=E)
+
+label3 = Label( topFrame, text="Välj Katigorien :")
+label3.grid(row=2, sticky=E )
+
+TextINP1= Entry(topFrame, )
+TextINP1.grid(row=0, column=1 )
+
+Checkbox1= Checkbutton(topFrame, text="Medium")
+Checkbox1.grid(row=1, column=1, sticky=E)
+Checkbox2= Checkbutton(topFrame, text="Easy" )
+Checkbox2.grid(row=1, column=1, sticky=W)
+Checkbox3= Checkbutton(topFrame, text="Hard" )
+Checkbox3.grid(row=1, column=2, sticky=W)
+Checkbox4= Checkbutton(topFrame, text="Random" )
+Checkbox4.grid(row=1, column=3, sticky=W)
+
+TextINP3= Entry(topFrame, )
+TextINP3.grid(row=2, column=1 )
+
+knapp = Button(bottomFrame, text="Submit")
+knapp.pack()
+
+
+root.mainloop()
